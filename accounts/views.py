4869fa4197e6e7ccc26d864889f392396ebf4b73
PolMirasso <pol.mirasso@gmail.com>
2023-04-21 12:36:56 +0200

    User login get username, email, age and image_profile

--- accounts/views.py
+++ accounts/views.py
@@ -21,12 +21,36 @@ class RegisterAPI(generics.GenericAPIView):
         "token": AuthToken.objects.create(user)[1]
         })
     
+# class LoginAPI(KnoxLoginView):
+#     permission_classes = (permissions.AllowAny,)
+
+#     def post(self, request, format=None):
+#         serializer = AuthTokenSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         user = serializer.validated_data['user']
+#         login(request, user)
+#         return super(LoginAPI, self).post(request, format=None)
+    
+
+
 class LoginAPI(KnoxLoginView):
     permission_classes = (permissions.AllowAny,)
-
+  
     def post(self, request, format=None):
         serializer = AuthTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
+        user_name=user.username
+        user_email=user.email
+        user_age = getattr(user, 'age', None) 
+        user_image_profile = getattr(user, 'image_profile', 'https://wheretowatch-vps.herokuapp.com/static/defaultImageProfile.png')  
+
+
         login(request, user)
-        return super(LoginAPI, self).post(request, format=None)
+        temp_list=super(LoginAPI, self).post(request, format=None)
+        temp_list.data["username"]=user_name
+        temp_list.data["email"]=user_email
+        temp_list.data["age"]=user_age
+        temp_list.data["image_profile"]=user_image_profile
+
+        return Response({"user":temp_list.data})
