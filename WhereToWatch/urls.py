bb96f0853d966cb8708b64a4fa7b5f7f9c67b089
PolMirasso <pol.mirasso@gmail.com>
2023-04-24 13:15:20 +0200

    urls repath fix

--- WhereToWatch/urls.py
+++ WhereToWatch/urls.py
@@ -18,10 +18,17 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
-    path('', include('accounts.urls'))
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)    
+    path('', include('accounts.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 
