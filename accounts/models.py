d3a4a69a6a63773d63219ec2490e51721be699da
PolMirasso <pol.mirasso@gmail.com>
2023-05-11 11:18:58 +0200

    model migration

--- accounts/models.py
+++ accounts/models.py
@@ -9,7 +9,6 @@ class data_user(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nsfw_content = models.BooleanField(blank=True)
     image_profile = models.ImageField(blank=True)
-    email = models.EmailField(max_length=100, unique=True)
     description = models.CharField(max_length=500, null=True)
 
     def __str__(self):
