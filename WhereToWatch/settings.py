a5d79ff355d3361c5633720d8364519e8b41fb3f
PolMirasso <pol.mirasso@gmail.com>
2023-05-19 17:14:54 +0200

    fix front allowed url

--- WhereToWatch/settings.py
+++ WhereToWatch/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://wheretowatch-18a5f.web.app/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -73,6 +73,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
     'https://wheretowatch-18a5f.web.app/'
 ]
 
